3356c7b4bb446ef33b82f71c14e4b0a322498c79
sucaiji <sucaiji@aliyun.com>
2018-11-16 16:57:11 +0800

    更新，将原来的eruaka-client改造为服务提供者，添加resttemplate+ribbon作为服务消费者同时进行负载均衡

---
+++ service-ribbon/src/main/java/com/sucaiji/serviceribbon/HelloService.java
@@ -0,0 +1,16 @@
+package com.sucaiji.serviceribbon;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public String hiService(String name) {
+        return  restTemplate.getForObject("http://eureka-client/hi",  String.class);
+    }
+}
