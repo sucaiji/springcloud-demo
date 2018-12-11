412d87c511d9e968edc4c3f105d8f6483c66c376
sucaiji <sucaiji@aliyun.com>
2018-12-11 17:24:44 +0800

    添加安全验证

---
+++ service-zuul/src/main/java/com/sucaiji/servicezuul/MyFilter.java
@@ -0,0 +1,44 @@
+package com.sucaiji.servicezuul;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class MyFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        Object accessToken = request.getParameter("token");
+        if (accessToken == null) {
+            requestContext.setSendZuulResponse(false);
+            requestContext.setResponseStatusCode(401);
+            try {
+                requestContext.getResponse().getWriter().write("token is empty");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+}
